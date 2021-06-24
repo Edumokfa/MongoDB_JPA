ff4026a5dff390f0d9ec77c321577ef65862d6a6
Edumokfa <edumokfa@hotmail.com>
2021-06-24 09:25:18 -0300

    Projeto Finalizado

---
+++ src/main/java/com/systempro/workshopmongo/config/Instantiation.java
@@ -0,0 +1,62 @@
+package com.systempro.workshopmongo.config;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.TimeZone;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.systempro.workshopmongo.domain.Post;
+import com.systempro.workshopmongo.domain.User;
+import com.systempro.workshopmongo.dto.AuthorDTO;
+import com.systempro.workshopmongo.dto.CommentDTO;
+import com.systempro.workshopmongo.repository.PostRepository;
+import com.systempro.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userReposiroty;
+
+	@Autowired
+	private PostRepository postReposiroty;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		userReposiroty.deleteAll();
+		postReposiroty.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userReposiroty.save(maria);
+		userReposiroty.save(alex);
+		userReposiroty.save(bob);
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
+		postReposiroty.save(post1);
+		postReposiroty.save(post2);
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userReposiroty.save(maria);
+	}
+	
+
+}
---
+++ src/main/java/com/systempro/workshopmongo/services/PostService.java
@@ -0,0 +1,32 @@
+package com.systempro.workshopmongo.services;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.systempro.workshopmongo.domain.Post;
+import com.systempro.workshopmongo.repository.PostRepository;
+import com.systempro.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+	
+	public List<Post> findByTitle(String text) {
+		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
+}
